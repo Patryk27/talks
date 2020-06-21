037cc2fc5d4969b6c1025f3edd4a943d1cfbe86c
Patryk Wychowaniec <wychowaniec.patryk@gmail.com>
2020-06-21 12:00:42 +0200

    theme: Remove `font-awesome`

--- 2020-fantastic-actors/template.js
+++ 2020-fantastic-actors/template.js
@@ -150,7 +150,6 @@ module.exports = {
         <head>
             <meta charset="UTF-8"/>
             <link rel="stylesheet" href="/mnt/common/theme/css/asciidoctor.css">
-            <link rel="stylesheet" href="/mnt/common/theme/css/font-awesome.css">
             <link rel="stylesheet" href="/mnt/common/theme/css/highlighting-github.css">
             <link rel="stylesheet" href="/mnt/common/theme/css/talk.css">
             <script src="/mnt/common/theme/js/highlight.js"></script>
